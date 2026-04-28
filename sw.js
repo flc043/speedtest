2be3e9b23026e004890d5a3f2e108643515ab56b
flc043 <37825968+flc043@users.noreply.github.com>
2026-04-28 20:25:44 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,18 @@
-const CACHE_NAME = 'speedtest-v3';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './192.png',
-  './512.png'
-];
+const CACHE_NAME = 'speedtest-v5';
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting(); // 強制更新
 });
 
 self.addEventListener('fetch', (event) => {
-    const url = event.request.url;
+  // 如果是測速請求，直接跳過 Service Worker，走原生網路
+  if (event.request.url.includes('cloudflare.com')) {
+    return; 
+  }
 
-    // 只要網址包含 cloudflare，完全不處理，直接交給瀏覽器原生網路
-    if (url.includes('cloudflare.com')) {
-        return;
-    }
-
-    // 只快取 GET 請求
-    if (event.request.method !== 'GET') return;
-
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
